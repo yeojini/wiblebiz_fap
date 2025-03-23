564d1c96ae05197952956d7ff6198644aa9049a4
yeojini <yesjin1209@gmail.com>
2025-03-24 01:29:27 +0900

    chore: ESLint 및 VSCode 설정 업데이트
    - ESLint 설정을 개선하여 prettier 및 jsx-a11y 플러그인 추가
    - VSCode 설정 파일을 추가하여 저장 시 ESLint 자동 수정 기능 활성화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,54 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
   ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+      'plugin:jsx-a11y/recommended',
+    ],
+    plugins: ['prettier', 'jsx-a11y', 'no-relative-import-paths'],
+    settings: {
+      react: {
+        version: '19.0.0',
+      },
+    },
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          semi: true,
+          tabWidth: 2,
+          singleQuote: true,
+          printWidth: 80,
+          endOfLine: 'auto',
+          arrowParens: 'always',
+        },
+        {
+          usePrettierrc: false,
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+      'no-relative-import-paths/no-relative-import-paths': [
+        'warn',
+        {
+          allowSameFolder: true,
+        },
+      ],
+    },
   }),
 ];
 
