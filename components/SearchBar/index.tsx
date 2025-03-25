992fd16ea76f348b7c94de45d54d161f347f8521
yeojini <yesjin1209@gmail.com>
2025-03-26 04:35:52 +0900

    feat: 검색바 컴포넌트에 클리어 아이콘 추가 및 스타일링 개선
    - ClearIcon SVG 파일 추가
    - 검색바에 클리어 버튼 기능 구현
    - SearchBar.module.scss 파일 생성 및 스타일 정의

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -1,14 +1,19 @@
+import { useId } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { useSearchContext } from '@/hooks/useSearchContext';
 import { type SearchFormData } from '@/schemas/searchSchema';
 import SearchIcon from '@/assets/icons/search_icon.svg';
-import { useSearchContext } from '@/hooks/useSearchContext';
-import { useId } from 'react';
+import ClearIcon from '@/assets/icons/clear_icon.svg';
+import styles from './SearchBar.module.scss';
 
 export default function SearchBar() {
   const searchInputId = useId();
   const { setQuery } = useSearchContext();
 
-  const { register, handleSubmit } = useFormContext<SearchFormData>();
+  const { register, handleSubmit, watch, setValue } =
+    useFormContext<SearchFormData>();
+
+  const search = watch('search');
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,20 +22,37 @@ export default function SearchBar() {
     })(e);
   };
 
+  const handleClear = () => {
+    setValue('search', '');
+  };
+
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} className={styles.container}>
       <label htmlFor={searchInputId} className="sr-only">
         검색
       </label>
-      <input
-        id={searchInputId}
-        type="text"
-        placeholder="찾으시는 내용을 입력해 주세요"
-        {...register('search')}
-      />
-      <button type="submit" aria-label="검색하기">
-        <SearchIcon />
-      </button>
+      <div className={styles.inputContainer}>
+        <input
+          id={searchInputId}
+          type="text"
+          placeholder="찾으시는 내용을 입력해 주세요"
+          className={styles.input}
+          {...register('search')}
+        />
+        {search && (
+          <button
+            className={styles.button}
+            type="button"
+            aria-label="지우기"
+            onClick={handleClear}
+          >
+            <ClearIcon width={24} height={24} />
+          </button>
+        )}
+        <button className={styles.button} type="submit" aria-label="검색하기">
+          <SearchIcon width={28} height={28} />
+        </button>
+      </div>
     </form>
   );
 }
