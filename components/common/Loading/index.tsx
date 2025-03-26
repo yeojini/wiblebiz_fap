795d01dbc18624811c1bcbfe7a7b1abc32f58aa7
yeojini <yesjin1209@gmail.com>
2025-03-26 21:39:26 +0900

    feat: Loading 컴포넌트 및 스타일 추가
    - Loading 컴포넌트 구현 및 Loading.module.scss 파일 생성
    - BeatLoader를 사용하여 로딩 UI 추가

---
+++ components/common/Loading/index.tsx
@@ -0,0 +1,16 @@
+import { BeatLoader } from 'react-spinners';
+import styles from './Loading.module.scss';
+
+type LoadingProps = {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+export default function Loading({ className, children }: LoadingProps) {
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <BeatLoader />
+      {children}
+    </div>
+  );
+}
