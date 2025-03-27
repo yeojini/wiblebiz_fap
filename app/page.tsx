38d31c3d057cefe724a3c785d30efd5daced470d
yeojini <yesjin1209@gmail.com>
2025-03-27 18:43:51 +0900

    chore: Home 컴포넌트에서 동적 렌더링 설정 추가
    - 빌드 실패 오류를 해결하기 위해 Home 컴포넌트에 'force-dynamic' 설정을 추가하여 동적 렌더링을 강제함

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import ServiceInquirySection from '@/components/ServiceInquirySection';
 import ServiceGuideSection from '@/components/ServiceGuideSection';
 import AppDownloadSection from '@/components/AppDownloadSection';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <main>
